552f6cc3b8e480e4184c3941bbb6e6a1c2cf7710
dageon <93782184+dageon@users.noreply.github.com>
2023-10-05 15:19:15 +0900

    2

--- main.c
+++ main.c
@@ -7,12 +7,10 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오 :");
 	scanf("%i", &a);
 	
-	if(a > 0)
-		printf("양수입니다.\n");
-	else if(a < 0)
-		printf("음수입니다.\n");
+	if(a < 0)
+		printf("절대값은 %i 입니다.\n", -a);
 	else
-		printf("0 입니다.\n");
+		printf("절대값은 %i 입니다.\n", a);
 	
 	return 0;
 }
